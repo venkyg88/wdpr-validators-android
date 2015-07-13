2b56897cbb07c9c3acc1d4c31194d572683c7be9
Venkat Gonuguntala <venkyg88@gmail.com>
2015-07-13 10:37:46 -0400

    template added

---
+++ WDPRValidators/src/main/java/com/disney/android/wdprvalidators/UrlValidator.java
@@ -0,0 +1,15 @@
+package com.disney.android.wdprvalidators;
+
+/**
+ * Created by venkatgonuguntala on 7/8/15.
+ */
+public class UrlValidator {
+    /*String[] schemes = {"http","https"};
+    UrlValidator urlValidator = new UrlValidator(schemes);
+    if (urlValidator.isValid("ftp://foo.bar.com/")) {
+        System.out.println("url is valid");
+    }
+    else {
+        System.out.println("url is invalid");
+    }*/
+}
---
+++ WDPRValidators/src/test/java/com/disney/android/wdprvalidators/UrlValidatorTest.java
@@ -0,0 +1,8 @@
+package com.disney.android.wdprvalidators;
+
+/**
+ * Created by venkatgonuguntala on 7/8/15.
+ */
+public class UrlValidatorTest {
+    
+}
