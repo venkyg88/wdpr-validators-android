2a1056e94153c1a24a823273a02b65afeeb910ce
Venkat Gonuguntala <venkyg88@gmail.com>
2015-09-28 15:23:41 -0400

    adding initial demo files

--- WDPRValidatorsDemo/src/main/java/com/disney/android/wdprvalidatorsdemo/ObjectLengthValidatorDemo.java
+++ WDPRValidatorsDemo/src/main/java/com/disney/android/wdprvalidatorsdemo/ObjectLengthValidatorDemo.java
@@ -1,10 +1,17 @@
 package com.disney.android.wdprvalidatorsdemo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by venkatgonuguntala on 9/28/15.
  */
 public class ObjectLengthValidatorDemo extends Activity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objlength);
+
+    }
 }
