40689a3c76ae159203445680246e5883f99cdd01
Venkat Gonuguntala <venkyg88@gmail.com>
2015-09-28 15:14:24 -0400

    Initial commits

---
+++ WDPRValidatorsDemo/src/main/java/com/disney/android/wdprvalidatorsdemo/ObjectLengthValidatorDemo.java
@@ -0,0 +1,10 @@
+package com.disney.android.wdprvalidatorsdemo;
+
+import android.app.Activity;
+
+/**
+ * Created by venkatgonuguntala on 9/28/15.
+ */
+public class ObjectLengthValidatorDemo extends Activity{
+
+}
